e2be6028320d5c5002a0319344d21ab8fd5b19c6
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-11-16 19:31:31 -0300

    Projeto react + eslint + prettier + editorConfig

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const foo = 'bar';
+export const App: React.FC = () => {
+    return (
+        <>
+            <h2>Hello world</h2>
+        </>
+    );
+};
+
+export default App;
